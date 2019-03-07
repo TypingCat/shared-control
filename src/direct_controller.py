4aaa29f8966983883060f2bab65c929346be47cf
fini <finiel@naver.com>
2019-03-07 10:39:44 +0900

    직접제어 통합

--- src/direct_controller.py
+++ src/direct_controller.py
@@ -11,27 +11,21 @@ import tf
 
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Joy
 
 
-class DirectController:
-    """키보드로 이동로봇을 직접 제어한다"""
+class DirectControl:
+    """이동로봇을 직접 제어한다"""
     def __init__(self):
         self.robot_vel_lin = rospy.get_param('~robot_vel_lin', 0.26)
+        self.robot_vel_ang = rospy.get_param('~robot_vel_ang', 1.82)
 
-        self.robot_yaw = 0.
-        rospy.Subscriber('odom', Odometry, self.update_robot_yaw)
+        self.publisher_cmd_vel = rospy.Publisher('cmd_vel', Twist, queue_size=1)
 
-        self.publisher_cmd_vec = rospy.Publisher('cmd_vec', Twist, queue_size=1)
+        rospy.Subscriber('joy', Joy, self.joystick)
 
         self.key_setting = termios.tcgetattr(sys.stdin)
-        self.key_watcher = rospy.Timer(rospy.Duration(rospy.get_param('~freq', 0.1)), self.spin)
-
-    def update_robot_yaw(self, data):
-        """로봇의 자세를 갱신한다"""
-        (_, _, self.robot_yaw) = tf.transformations.euler_from_quaternion([data.pose.pose.orientation.x,
-                                                  data.pose.pose.orientation.y,
-                                                  data.pose.pose.orientation.z,
-                                                  data.pose.pose.orientation.w,])
+        self.key_watcher = rospy.Timer(rospy.Duration(rospy.get_param('~freq', 0.1)), self.keyboard)
 
     def get_key(self):
         """키보드 입력을 획득한다"""
@@ -41,29 +35,40 @@ class DirectController:
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.key_setting)
         return key
 
-    def spin(self, event):
-        """키보드 입력을 BCI 프로토콜로 변환한다"""
-        cmd_vec = Twist()
+    def keyboard(self, event):
+        """키보드 입력을 제어명령으로 변환한다"""
+        cmd_vel = Twist()
         key = self.get_key()
         if key == '\x03':   # ctrl+c
             self.key_watcher.shutdown()
         elif key == 'a':    # left
-            cmd_vec.linear.x = self.robot_vel_lin
-            cmd_vec.angular.z = self.robot_yaw
+            cmd_vel.linear.x = 0.
+            cmd_vel.angular.z = self.robot_vel_ang
         elif key == 'w':    # forward
-            cmd_vec.linear.x = self.robot_vel_lin
-            cmd_vec.angular.z = 0.
+            cmd_vel.linear.x = self.robot_vel_lin
+            cmd_vel.angular.z = 0.
         elif key == 'd':    # right
-            cmd_vec.linear.x = self.robot_vel_lin
-            cmd_vec.angular.z = -self.robot_yaw
+            cmd_vel.linear.x = 0.
+            cmd_vel.angular.z = -self.robot_vel_ang
         elif key == 's':    # stop
-            cmd_vec.linear.x = 0.
-            cmd_vec.angular.z = 0.
+            cmd_vel.linear.x = 0.
+            cmd_vel.angular.z = 0.
+        elif key == 'x':    # barkward
+            cmd_vel.linear.x = -self.robot_vel_lin
+            cmd_vel.angular.z = 0.
 
-        self.publisher_cmd_vec.publish(cmd_vec)
+        self.publisher_cmd_vel.publish(cmd_vel)
+
+    def joystick(self, data):
+        """조이스틱 입력을 제어명령으로 변환한다"""
+        cmd_vel = Twist()
+        cmd_vel.linear.x = self.robot_vel_lin*data.axes[1]
+        cmd_vel.angular.z = self.robot_vel_ang*data.axes[0]
+
+        self.publisher_cmd_vel.publish(cmd_vel)
 
 
 if __name__ == '__main__':
     rospy.init_node('direct_controller')
-    dc = DirectController()
+    dc = DirectControl()
     rospy.spin()
